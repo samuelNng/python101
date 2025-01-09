babb356534c77c13fd63dd459ae2cb824d02144e
samuelNng <nsamuelmnng@gmail.com>
2025-01-09 14:28:31 +0000

    create python practice flie

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+
+
+
+n1 = int(input("write a number: ") )
+
+if n1%2==1:
+    print ('n1 is odd')
+else:
+    print ('n1 is even')
--- main.py
+++ main.py
@@ -1,2 +1,35 @@
 print("Hello World!")
 print("Hello World!")
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    # Write your code here
+    #for i in ar:
+    
+    return ar[i]+ar[i+1]
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
